fee6b80a47f184527be49f7dd0997bb8ef9ea8e7
glecaros <gerardo.lecaros.e@gmail.com>
2017-09-10 19:52:51 -0700

    +string_tokenizer

---
+++ src/util/string_tokenizer.h
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <string>
+
+namespace Util
+{
+  namespace Internal
+  {
+    template<char delim>
+    void tok(std::istringstream &iss, std::string &s)
+    {
+      std::getline(iss, s, delim);
+    }
+
+    template<char delim, typename... Ts>
+    void tok(std::istringstream &iss, std::string &s, Ts&... args)
+    {
+      tok<delim>(iss, s);
+      tok<delim>(iss, args...);
+    }  
+  }
+
+  template<char delim, typename... Ts>
+  void tokenize(std::string &src, Ts&... args)
+  {
+    std::istringstream iss(src);
+    Internal::tok<delim>(iss, args...);
+  }
+}
